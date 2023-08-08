05ac0f98a6451f6de0eeb27f7024159c857ad7b7
motomotoyoshi <motomoto.git@gmail.com>
2023-08-09 00:56:25 +0900

    3-4

--- my-project/src/my_project/core.clj
+++ my-project/src/my_project/core.clj
@@ -1,9 +1,7 @@
 (ns my-project.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-int [str]
+  (Integer. str))
 
-(require '[my-project.hoge :as hoge])
-(hoge/inc100 24)
+(defn -main [& args]
+  (println "Sum: " (reduce + (map parse-int args))))
