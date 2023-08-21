b63785c132d5de2b84e50c5d3d84678250298811
motomotoyoshi <motomoto.git@gmail.com>
2023-08-22 02:11:28 +0900

    routing

--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -21,3 +21,55 @@
   (when @server
     (stop-server)
     (start-server)))
+
+(def routes
+  {"/" home
+   "/todo" todo-index})
+
+(defn ok [body]
+  {:status 200
+   :body body})
+
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf8"}))
+
+(defn not-found []
+  {:status 404
+   :body "<h1>404 page not found</h1>"})
+
+(defn home-view [req]
+  "<h1>ホーム画面</h1>
+   <a href=\"/todo\">TODO 一覧</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+(def todo-list
+  [{:title "朝ごはんを作る"}
+   {:title "燃えるゴミを出す"}
+   {:title "卵を買って帰る"}
+   {:title "お風呂を洗う"}])
+
+(defn todo-index-view [req]
+  `("<h1>TODO 一覧</h1>"
+     "<ul>"
+     ~@(for [{:keys [title]} todo-list]
+         (str "<li>" title "</li>"))
+     "</ul>"))
+
+(defn todo-index [req]
+  (-> (todo-index-view req)
+      ok
+      html))
+
+(defn match-route [uri]
+  (get routes uri))
+
+(defn handler [req]
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+      (maybe-fn req)
+      (not-found))))
