012b7d7176c8845f250872136a87c1c75c07f12e
motomotoyoshi <motomoto.git@gmail.com>
2023-08-24 01:06:51 +0900

    Part3 end

--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -1,16 +1,19 @@
 (ns todo-clj.core
-  (:require [ring.adapter.jetty :as server]))
+  (:require [compojure.core :refer [routes]]
+            [ring.adapter.jetty :as server]
+            [todo-clj.handler.main :refer [main-routes]]
+            [todo-clj.handler.todo :refer [todo-routes]]))
 
 (defonce server (atom nil))
 
-(defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "<h1>Hello, world!!</h1>"})
+(def app
+  (routes
+    todo-routes
+    main-routes))
 
 (defn start-server []
   (when-not @server
-    (reset! server (server/run-jetty #'handler {:port 3000 :join? false}))))
+    (reset! server (server/run-jetty #'app {:port 3000 :join? false}))))
 
 (defn stop-server []
   (when @server
@@ -21,55 +24,3 @@
   (when @server
     (stop-server)
     (start-server)))
-
-(def routes
-  {"/" home
-   "/todo" todo-index})
-
-(defn ok [body]
-  {:status 200
-   :body body})
-
-(defn html [res]
-  (assoc res :headers {"Content-Type" "text/html; charset=utf8"}))
-
-(defn not-found []
-  {:status 404
-   :body "<h1>404 page not found</h1>"})
-
-(defn home-view [req]
-  "<h1>ホーム画面</h1>
-   <a href=\"/todo\">TODO 一覧</a>")
-
-(defn home [req]
-  (-> (home-view req)
-      ok
-      html))
-
-(def todo-list
-  [{:title "朝ごはんを作る"}
-   {:title "燃えるゴミを出す"}
-   {:title "卵を買って帰る"}
-   {:title "お風呂を洗う"}])
-
-(defn todo-index-view [req]
-  `("<h1>TODO 一覧</h1>"
-     "<ul>"
-     ~@(for [{:keys [title]} todo-list]
-         (str "<li>" title "</li>"))
-     "</ul>"))
-
-(defn todo-index [req]
-  (-> (todo-index-view req)
-      ok
-      html))
-
-(defn match-route [uri]
-  (get routes uri))
-
-(defn handler [req]
-  (let [uri (:uri req)
-        maybe-fn (match-route uri)]
-    (if maybe-fn
-      (maybe-fn req)
-      (not-found))))
