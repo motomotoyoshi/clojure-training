4bf9abe957de2f5146e9f1af2cdafb465042ee25
motomotoyoshi <motomoto.git@gmail.com>
2023-08-25 01:49:40 +0900

    chapter3.5 end

--- todo-clj/project.clj
+++ todo-clj/project.clj
@@ -5,5 +5,10 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring "1.4.0"]
-                 [compojure "1.4.0"]]
-  :repl-options {:init-ns todo-clj.core})
+                 [compojure "1.4.0"]
+                 [environ "1.0.1"]]
+  :plugins [[lein-environ "1.0.1"]]
+  :repl-options {:init-ns todo-clj.core}
+  :profiles
+  {:dev {:dependencies [[prone "0.8.2"]]
+         :env {:dev true}}})
--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -2,14 +2,24 @@
   (:require [compojure.core :refer [routes]]
             [ring.adapter.jetty :as server]
             [todo-clj.handler.main :refer [main-routes]]
-            [todo-clj.handler.todo :refer [todo-routes]]))
+            [todo-clj.handler.todo :refer [todo-routes]]
+            [todo-clj.middleware :refer [wrap-dev]]
+            [environ.core :refer [env]]))
+
+(defn- wrap [handler middleware opt]
+  (if (true? opt)
+    (middleware handler)
+    (if opt
+      (middleware handler opt)
+      handler)))
 
 (defonce server (atom nil))
 
 (def app
-  (routes
+  (-> (routes
     todo-routes
-    main-routes))
+    main-routes)
+      (wrap wrap-dev (:dev env))))
 
 (defn start-server []
   (when-not @server
