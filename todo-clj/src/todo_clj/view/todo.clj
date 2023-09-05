015d81223efe1733c01ebff328e3d3020f13f7cd
motomotoyoshi <motomoto.git@gmail.com>
2023-09-06 01:42:54 +0900

    Chapter6, todo detail

--- todo-clj/src/todo_clj/view/todo.clj
+++ todo-clj/src/todo_clj/view/todo.clj
@@ -22,3 +22,10 @@
   (->> [:section.card
         [:h2 "TODO を追加しました!!"]]
        (layout/common req)))
+
+(defn todo-show-view [req todo]
+  (->> [:section.card
+        (when-let [{:keys [msg]} (:flash req)]
+          [:div.alert.alert-success [:strong msg]])
+        [:h2 (:title todo)]]
+       (layout/common req)))
