34f0af42e39d3e9392c89e2206e199fa5644afc3
motomotoyoshi <motomoto.git@gmail.com>
2023-09-07 02:12:43 +0900

    Chapter6, add todo edit

--- todo-clj/src/todo_clj/db/todo.clj
+++ todo-clj/src/todo_clj/db/todo.clj
@@ -10,3 +10,6 @@
 
 (defn find-first-todo [id]
   (first (jdbc/query db/db-spec ["select * from todo where id = ?" id])))
+
+(defn update-todo [id title]
+  (jdbc/update! db/db-spec :todo {:title title} ["id = ?" id]))
